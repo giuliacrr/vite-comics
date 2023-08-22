90dd53e11a524181cb6ebbe4d24ed98fd2079e9d
giuliacrr <giulia.corrado02@gmail.com>
2023-08-22 17:07:21 +0200

    buy section done - footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,50 +4,92 @@ export default {};
 
 <template>
   <div class="bg-primary">
+    <!--Start buy section-->
     <div class="container">
-      <div class="d-flex pt-2 pb-2">
-        <div class="buy-icons">
+      <div class="d-flex pt-5 pb-5 justify-content-between">
+        <div class="d-flex align-items-center">
           <img
             class="buy-icons"
             src="../assets/img/buy-comics-digital-comics.png"
             alt="digital comics"
           />
+          <div class="ms-2">
+            <span class="text-uppercase text-white">digital comics</span>
+          </div>
         </div>
-        <div>
+        <!---->
+        <div class="d-flex align-items-center">
           <img
             class="buy-icons"
             src="../assets/img/buy-comics-merchandise.png"
             alt="digital comics"
           />
+          <div class="ms-2">
+            <span class="text-uppercase text-white">dc merchandise</span>
+          </div>
         </div>
-        <div>
+        <!---->
+        <div class="d-flex align-items-center">
           <img
             class="buy-icons"
             src="../assets/img/buy-comics-shop-locator.png"
             alt="digital comics"
           />
+          <div class="ms-2">
+            <span class="text-uppercase text-white">subscription</span>
+          </div>
         </div>
-        <div>
+        <!---->
+        <div class="d-flex align-items-center">
           <img
             class="buy-icons"
             src="../assets/img/buy-comics-subscriptions.png"
             alt="digital comics"
           />
+          <div class="ms-2">
+            <span class="text-uppercase text-white">comic shop locator</span>
+          </div>
         </div>
-        <div>
+        <!---->
+        <div class="d-flex align-items-center">
           <img
-            class="buy-icons"
+            class="buy-icons svg"
             src="../assets/img/buy-dc-power-visa.svg"
             alt="digital comics"
           />
+          <div class="ms-2">
+            <span class="text-uppercase text-white">dc power visa</span>
+          </div>
         </div>
       </div>
     </div>
+    <!--End buy section-->
+    <!--Start info section-->
+    <div class="info-bg">
+      <div class="container">
+        <div>lists</div>
+        <div><img src="../assets/img/dc-logo-bg.png" alt="dclogobg" /></div>
+      </div>
+    </div>
+    <!--End info section-->
+
+    <!--Start Sign-up and follow-up section-->
+    <!--End Sign-up and follow-up section-->
   </div>
 </template>
 
 <style lang="scss" scoped>
 .buy-icons {
-  height: 80px;
+  height: 50px;
+}
+.svg {
+  width: 50px;
+}
+
+.info-bg {
+  min-height: 300px;
+  background-image: url(../assets/img/footer-bg.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
 }
 </style>
